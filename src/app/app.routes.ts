0db90bc6397d3de2d77d5a601082f203bfc51518
ozgurakgul <ozgurakgul12345@gmail.com>
2025-05-09 15:05:26 +0300

    Initial commit: Project structure setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {CampaignListComponent} from "./modules/campaign/campaign-list/campaign-list.component";
+import {LoginComponent} from "./modules/auth/login/login.component";
+import {authGuard} from "./core/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+        {
+        path: 'campaign',
+        component: CampaignListComponent,
+        canActivate: [authGuard],
+    },
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: '**', redirectTo: '/login'}];
---
+++ src/app/modules/campaign/campaign-list/campaign-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign-list.component.html',
+  styleUrl: './campaign-list.component.scss'
+})
+export class CampaignListComponent {
+
+}
