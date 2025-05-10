1a042d188d5cf73d66feefbfd69ebbab4979e099
ozgurakgul <ozgurakgul12345@gmail.com>
2025-05-10 14:45:18 +0300

    Added toggle button to header, redesigned sidebar component, and routed to list and create components

---
+++ src/app/modules/campaign/campaign-create/campaign-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign-create.component.html',
+  styleUrl: './campaign-create.component.scss'
+})
+export class CampaignCreateComponent {
+
+}
